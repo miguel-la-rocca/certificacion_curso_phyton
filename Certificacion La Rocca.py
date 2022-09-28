27f40f52a768ee327d41f9c9d4b9c64cdf66b387
miguel-la-rocca <108734320+miguel-la-rocca@users.noreply.github.com>
2022-09-27 23:32:21 -0300

    Add files via upload

---
+++ Certificacion La Rocca.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 16:46:45 2022
+
+@author: gaita_c0xadhf
+"""
+
+
+bicicletas=[]
+class Bicicleteria:
+    def __init__(self , bicicletas , ganancias , cantidad_de_ventas ):
+        
+        
+        self.bicicletas=bicicletas[bicicleta]
+        self.ganancias=float(ganancias)
+        self.cantidad_de_ventas=cantidad_de_ventas
+        
+        objbicicleta = Bicicleta(n_serie, modelo, valor)
+        bicicletas.append(objbicicleta)
+        
+        
+       
+    def vender_bicicleta (self , Bicicleta):
+        
+        self.cantidad_de_ventas+=1
+        self.ganancias+= self.bicicleta [0]
+        return 
+        
+    def comprar_bicicleta(Bicicleta):
+        
+        print("Compra de bicicleta\n")
+        n_serie = int(input("Nro de numero de serie: "))
+        valor = float(input("Valor: "))
+        modelo = input("Modelo: ")
+        objbicicleta = Bicicleta(n_serie, valor, modelo)
+        bicicletas.append(objbicicleta)
+        
+class Bicicleta:
+    
+
+    def __init__(self , n_serie , valor , modelo   ):
+        
+        self.n_serie=n_serie
+        self.valor=valor
+        self.modelo=modelo
+    def srt (Bicicleta):
+        print(Bicileta)
+        
+    def __setitem__(self , n_serie , valor):
+      
+      self.bicicleta[n_serie] = valor
+      
+      return self.bicicleta.get(n_serie, "No se encontro la bicicleta indicada")
+  
+    def __getprecio__(self,valor):  
+        
+        return self.bicicleta.valor
+    
+    def __getn_serie__(self,n_serie):  
+        
+        return self.bicicleta.n_serie
+    
+    
+    
+Bicicleta1=Bicicleta(121, 50000 , "fx54")     
+Bicicleteria.comprar_bicicleta(Bicicleta)    
+   
+    
+   
+    
+    
+    bicicletas=[]
+    
+    #bicicleta=[n_serie , valor ,modelo]
+    bicicletas=[bicicleta]  
+    print(bicicleta)
+    print(bicicletas)
+    
+    
+bicicleta=[]
+bicicletas=[bicicleta]
+Bicicleta.__setitem__( 121, 50000)        
+    
+
+Bicicleta1=Bicicleta(121, 50000 , "fx54") 
+Bicicleta2=Bicicleta(122, 65000 , "fd5") 
+Bicicleta3=Bicicleta(123, 80000 , "xd4") 
+Bicicleta4=Bicicleta(124, 20000 , "x54") 
+
+Ludmila_bicicletas=Bicicleteria( bicicletas, 0, 0)
+Ludmila_bicicletas.
+       
+Bicicleta1.getprecio()
+        
+Bicicleta1.valor 
+Bicicleta1.n_serie       
+        
+        
+Bicicleteria(bicicleta1, ganancias, cantidad_de_ventas)         
+        
+        
